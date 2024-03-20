bb0e47c145e04bb43deb7af25ca26ebfa4422a7f
simoneevacca <v.simone.vacca@gmail.com>
2024-03-20 14:41:13 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+
+  export default {
+
+    data() {
+      return {
+        message: 'hello world'
+      }
+    }
+  }
+
+</script>
+  <div>{{message}}</div>
+<template>
+  
+</template>
+
+<style>
+
+</style>
